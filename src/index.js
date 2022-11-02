34226571a4cb51361dff30d041f455d2d4533ef5
Juan Mosquella <jmosquella11@gmail.com>
2022-11-02 20:06:02 -0300

    agregada pagina usuario

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import User from "./pages/User";
 
 disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="/user" element={<User />} />
+    </Routes>
+  </BrowserRouter>
 );
