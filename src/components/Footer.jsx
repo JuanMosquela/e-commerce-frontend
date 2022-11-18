f64339eec695daeac4ae37018e23a44255e8e7f2
Juan Mosquella <jmosquella11@gmail.com>
2022-11-18 01:00:27 -0300

    footer creado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,56 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="pb-10">
+      <div className="container grid grid-cols-5">
+        <div>
+          <Link
+            className=" py-1  font-bold uppercase text-sm md:text-md lg:text-xl text-black  "
+            to="/"
+          >
+            <span className="text-orange-400 text-xl font-bold">physical </span>
+            point
+          </Link>
+        </div>
+        <ul className=" flex flex-col gap-3 ">
+          <li className="uppercase text-slate-700 text-sm font-semibold">
+            solutions
+          </li>
+          <li className="capitalize text-slate-700 text-sm">marketing</li>
+          <li className="capitalize text-slate-700 text-sm">analytics</li>
+          <li className="capitalize text-slate-700 text-sm">commerce</li>
+          <li className="capitalize text-slate-700 text-sm">insights</li>
+        </ul>
+        <ul className="flex flex-col gap-3">
+          <li className="uppercase text-slate-700 text-sm font-semibold">
+            solutions
+          </li>
+          <li className="capitalize text-slate-700 text-sm">marketing</li>
+          <li className="capitalize text-slate-700 text-sm">analytics</li>
+          <li className="capitalize text-slate-700 text-sm">commerce</li>
+          <li className="capitalize text-slate-700 text-sm">insights</li>
+        </ul>
+        <ul className=" flex flex-col gap-3 ">
+          <li className="uppercase text-slate-700 text-sm font-semibold">
+            solutions
+          </li>
+          <li className="capitalize text-slate-700 text-sm">marketing</li>
+          <li className="capitalize text-slate-700 text-sm">analytics</li>
+          <li className="capitalize text-slate-700 text-sm">commerce</li>
+          <li className="capitalize text-slate-700 text-sm">insights</li>
+        </ul>
+        <ul className=" flex flex-col gap-3">
+          <li className="uppercase text-slate-700 text-sm font-semibold">
+            solutions
+          </li>
+          <li className="capitalize text-slate-700 text-sm">marketing</li>
+          <li className="capitalize text-slate-700 text-sm">analytics</li>
+          <li className="capitalize text-slate-700 text-sm">commerce</li>
+          <li className="capitalize text-slate-700 text-sm">insights</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 export default Footer;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,12 @@ import servicesInfo from "../utils/servicesData";
 
 const Services = () => {
   return (
-    <div className=" container flex justify-between items-center min-h-[20vh] ">
+    <div className=" container flex justify-between items-center min-h-[20vh] gap-6">
       {servicesInfo.map((service, index) => (
-        <div className="flex items-center justify-center gap-6 border-gray-200 border-2 p-2 flex-1">
+        <div
+          key={index}
+          className="flex items-center justify-center gap-6 border-orange-200 border-2 p-2 flex-1"
+        >
           <i className="text-2xl">{service.icon}</i>
 
           <div>
