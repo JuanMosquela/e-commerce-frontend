8a079cec6529fd16d8934fc8de56bff7f0257762
Juan Mosquella <jmosquella11@gmail.com>
2023-01-19 16:58:53 -0300

    creando page order

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -12,28 +12,30 @@ const Orders = () => {
     <section className="md:container min-height">
       <h1>Your orders information</h1>
 
-      {data?.map((order) => (
-        <div key={order._id}>
-          <h3>Order id: {order?.orderId}</h3>
+      <div className="flex gap-4">
+        {data?.map((order) => (
+          <div key={order._id}>
+            <h3>Order id: {order?.orderId}</h3>
 
-          <h2>User email: {order?.email}</h2>
-          <p>Status: {order?.orderStatus}</p>
+            <h2>User email: {order?.email}</h2>
+            <p>Status: {order?.orderStatus}</p>
 
-          {order?.products?.map((item) => (
-            <div className="flex" key={item._id}>
-              <h2>{item?.product.title}</h2>
-              <div>
-                <h1>{item?.quantity}</h1>
-                <span>${item?.total}</span>
+            {order?.products?.map((item) => (
+              <div className="flex" key={item._id}>
+                <h2>{item?.product.title}</h2>
+                <div>
+                  <h1>{item?.quantity}</h1>
+                  <span>${item?.total}</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
 
-          <span>transaction amount: {order?.transactionAmount}</span>
+            <span>transaction amount: {order?.transactionAmount}</span>
 
-          <span>{order?.createdAt}</span>
-        </div>
-      ))}
+            <span>{order?.createdAt}</span>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
