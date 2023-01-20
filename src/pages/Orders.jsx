65b78e910bd0f1dc4c964b6b76bb75a782e8b274
Juan Mosquella <jmosquella11@gmail.com>
2023-01-19 21:25:48 -0300

    creada pagina de orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -9,30 +9,53 @@ const Orders = () => {
   console.log(data, error, isLoading);
 
   return (
-    <section className="md:container min-height">
-      <h1>Your orders information</h1>
+    <section className="md:container min-height md:my-10">
+      <h1 className="text-2xl  mb-4">Your orders information</h1>
 
-      <div className="flex gap-4">
+      <div className="border border-slate">
         {data?.map((order) => (
           <div key={order._id}>
-            <h3>Order id: {order?.orderId}</h3>
+            <div>
+              <h2 className="bg-gray p-2 text-lg ">Order Details</h2>
+              <ul className="flex flex-col gap-2 p-4">
+                <li>Order id: {order?.orderId}</li>
+                <li>User Email: {order?.email}</li>
+                <li className=" inline  ">
+                  Order Status:{" "}
+                  <span className="bg-blue px-2 py-1 text-white  rounded-md text-sm">
+                    {order?.orderStatus}
+                  </span>
+                </li>
+                <li>Created At: {order?.createdAt}</li>
+              </ul>
+            </div>
 
-            <h2>User email: {order?.email}</h2>
-            <p>Status: {order?.orderStatus}</p>
-
-            {order?.products?.map((item) => (
-              <div className="flex" key={item._id}>
-                <h2>{item?.product.title}</h2>
-                <div>
-                  <h1>{item?.quantity}</h1>
-                  <span>${item?.total}</span>
+            <div>
+              <h2 className="bg-gray p-2 text-lg ">Delivered Details</h2>
+              {order?.products?.map((item) => (
+                <div className="flex gap-4 p-4" key={item._id}>
+                  <img
+                    className="w-64"
+                    src={item.product.pictureURL}
+                    alt={item.product.title}
+                  />
+                  <div>
+                    <h2>{item?.product.title}</h2>
+                    <div>
+                      <h1>{item?.quantity}</h1>
+                      <span>${item?.total}</span>
+                    </div>
+                  </div>
                 </div>
+              ))}
+            </div>
+
+            <div>
+              <h2 className="bg-gray p-2 text-lg ">Order Total</h2>
+              <div className=" p-4">
+                <span>Subtotal: ${order?.transactionAmount}</span>
               </div>
-            ))}
-
-            <span>transaction amount: {order?.transactionAmount}</span>
-
-            <span>{order?.createdAt}</span>
+            </div>
           </div>
         ))}
       </div>
